6c88929403143ee4283ddf17b3df81752f142623
Saeideh <samoradi94@gmail.com>
2025-11-27 21:15:39 +0330

    Add method to SchemaAgent to fetch table data and test query execution

--- agent/schema_agent.py
+++ agent/schema_agent.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, inspect
 from agent.config import DATABASE_PATH
+import pandas as pd
 
 class SchemaAgent:
     def __init__(self, db_path = DATABASE_PATH):
@@ -12,6 +13,10 @@ class SchemaAgent:
     
     def describe_table(self, table_name):
         return self.inspector.get_columns(table_name)
+    
+    def fetch_table(self, table_name, limit = 5):
+        query = f" Select * from {table_name} LIMIT {limit}"
+        return pd.read_sql(query, con=self.engine)
 
         
         
@@ -24,3 +29,5 @@ if __name__ == '__main__':
     for table in agent.show_tables():
         print(f"Schema for table {table}")
         print(agent.describe_table(table))
+        print(f'Data Fetched from Table: {table}:')
+        print(agent.fetch_table(table_name=table))
